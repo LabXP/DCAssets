0bc7d43a2e072b02ff9297cbf66f891f117633b1
Ana <acrsaw@hotmail.com>
2015-07-02 18:12:28 -0300

    Level3
    ;—;

---
+++ Level3/ViewModel/PipeController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeController : MonoBehaviour {
+
+	Quaternion temp;
+	PontaController[] pontas;
+
+	public bool waterInPipe;
+	// Use this for initialization
+	void Awake () {
+		pontas = GetComponentsInChildren<PontaController>();
+	}
+	
+	// Update is called once per frame
+	public void Rotation () {
+		transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 90f);
+	}
+
+	public void ListennerWater(){
+		foreach(PontaController ponta in pontas){
+			if(ponta.getHaveWater){
+				waterInPipe = true;
+			}
+		}
+	}
+
+
+}
---
+++ Level3/ViewModel/PontaController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PontaController : MonoBehaviour {
+
+	private bool haveWater;
+
+	public bool getHaveWater (){
+		return haveWater;
+	}
+	public void setHaveWater(bool haveWather){
+		this.haveWater = haveWater;
+	}
+}
