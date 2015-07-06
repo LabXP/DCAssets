4de1343fab1aa853195fde9acad18c1666b2752e
Ana <acrsaw@hotmail.com>
2015-07-06 17:21:42 -0300

    bugGit

---
+++ Level3/ViewModel/PontaController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PontaController : MonoBehaviour {
+
+
+	public bool haveWater;
+
+	public bool getHaveWater () {
+		return haveWater;
+	}
+
+	public void setHaveWater(bool haveWater ){
+		this.haveWater = haveWater;
+	}
+}
